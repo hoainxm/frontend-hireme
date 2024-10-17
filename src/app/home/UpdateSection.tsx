663897958d4b74325560aa940fd98bbcbeecf41b
pnhnam <namm0704@gmail.com>
2024-10-17 17:36:34 +0700

    update updateSection

--- src/app/home/UpdateSection.tsx
+++ src/app/home/UpdateSection.tsx
@@ -1,4 +1,4 @@
-import React, { FC, HTMLAttributes, useEffect, useState, useTransition } from 'react';
+import React, { FC } from 'react';
 import { Palette } from '../../models/enum';
 import { useTranslation } from 'react-i18next';
 import { SectionLayout } from '../../common/ui/layout/section-layout';
@@ -7,44 +7,45 @@ import style from './home.module.scss';
 interface Props {
   sectionId: string;
 }
+
 export const UpdateSection: FC<Props> = (props) => {
   const { sectionId } = props;
   const { t } = useTranslation();
 
   return (
-    <SectionLayout id={sectionId} title='title.update' backgroundColor={Palette.WHITE}>
+    <SectionLayout id={sectionId} title={t('title.update')} backgroundColor={Palette.WHITE}>
       <div className={style.updateSection}>
         <div className={style.plan}>
-          <div className={style.headerBasic}>{t('level.basic')}</div>
+          <div className={style.headerBasic}>{t('basic.title')}</div>
           <ul className={style.features}>
-            <li>✔ {t('level.basic')}</li>
-            <li>✔ {t('level.basic')}</li>
-            <li>✘ {t('level.basic')}</li>
-            <li>✘ {t('level.basic')}</li>
+            <li>✔ {t('basic.advantage1')}</li>
+            <li>✔ {t('basic.advantage2')}</li>
+            <li>✘ {t('basic.disadvantage1')}</li>
+            <li>✘ {t('basic.disadvantage2')}</li>
           </ul>
-          <button className={style.basicButton}>{t('button.experience')}</button>
+          <button className={style.basicButton}>{t('basic.button')}</button>
         </div>
 
         <div className={style.plan}>
-          <div className={style.headerStandard}>{t('level.standard')}</div>
+          <div className={style.headerStandard}>{t('standard.title')}</div>
           <ul className={style.features}>
-            <li>✔ {t('level.basic')}</li>
-            <li>✔ {t('level.basic')}</li>
-            <li>✔ {t('level.basic')}</li>
-            <li>✘ {t('level.basic')}</li>
+            <li>✔ {t('standard.advantage1')}</li>
+            <li>✔ {t('standard.advantage2')}</li>
+            <li>✔ {t('standard.advantage3')}</li>
+            <li>✘ {t('standard.disadvantage1')}</li>
           </ul>
-          <button className={style.standardButton}>{t('button.buyNow')}</button>
+          <button className={style.standardButton}>{t('standard.button')}</button>
         </div>
 
         <div className={style.plan}>
-          <div className={style.headerPremium}>{t('level.premium')}</div>
+          <div className={style.headerPremium}>{t('premium.title')}</div>
           <ul className={style.features}>
-            <li>✔ {t('level.basic')}</li>
-            <li>✔ {t('level.basic')}</li>
-            <li>✔ {t('level.basic')}</li>
-            <li>✔ {t('level.basic')}</li>
+            <li>✔ {t('premium.advantage1')}</li>
+            <li>✔ {t('premium.advantage2')}</li>
+            <li>✔ {t('premium.advantage3')}</li>
+            <li>✔ {t('premium.advantage4')}</li>
           </ul>
-          <button className={style.premiumButton}>{t('button.buyNow')}</button>
+          <button className={style.premiumButton}>{t('premium.button')}</button>
         </div>
       </div>
     </SectionLayout>
